babdb20cd2f06781065ea3df386880d109044ea4
Ontree <wangyicmh@163.com>
2014-11-30 20:26:19 +0800

    bbs_in

--- src/com/edu/thss/smartdental/BBSInFragment.java
+++ src/com/edu/thss/smartdental/BBSInFragment.java
@@ -9,23 +9,23 @@ import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import android.support.v4.app.FragmentManager;
 
-public class BBSFragment extends Fragment {
+public class BBSInFragment extends Fragment {
 	
 	private RadioGroup radioGroup;
 	private FragmentManager fragmentManager;
 	
-	public BBSFragment() {
+	public BBSInFragment() {
 		
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_bbs, container,false);
+		View rootView = inflater.inflate(R.layout.fragment_bbs_in, container,false);
 		
 		fragmentManager = getFragmentManager();
-		radioGroup = (RadioGroup)rootView.findViewById(R.id.bbs_tab);
-		radioGroup.check(R.id.bbs_tab_choose);
+		radioGroup = (RadioGroup)rootView.findViewById(R.id.bbs_in_tab);
+		radioGroup.check(R.id.bbs_in_tab_view);
 		changeFragment(0);
 		radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
 
@@ -33,16 +33,25 @@ public class BBSFragment extends Fragment {
 			public void onCheckedChanged(RadioGroup group, int checkedId) {
 
 				switch(checkedId){
-				case R.id.bbs_tab_choose: 
+				case R.id.bbs_in_tab_view: 
 					changeFragment(0); 
 					break;
-				case R.id.bbs_tab_join:
+				case R.id.bbs_in_tab_select:
 					changeFragment(1);
 					break;
+				case R.id.bbs_in_tab_search:
+					changeFragment(2);
+					break;
+				case R.id.bbs_in_tab_news:
+					changeFragment(3);
+					break;
+				case R.id.bbs_in_tab_post:
+					changeFragment(4);
+					break;
 				//TODO ADD OTHER FRAGMENT
-				}      
+				}
+               
 			}} );
-		
 		
 		return rootView;
 	}
@@ -52,13 +61,23 @@ public class BBSFragment extends Fragment {
 		Fragment tempfragment = null;
 		switch(index){
 		case 0:
-			tempfragment = new BBSTabChooseFragment();
+			tempfragment = new BBSInTabViewFragment();
 			break;
 		case 1:
-			tempfragment = new BBSTabJoinFragment();
+			tempfragment = new BBSInTabSelectFragment();
+			break;
+		case 2:
+			tempfragment = new BBSInTabSearchFragment();
+			break;
+		case 3:
+			tempfragment = new BBSInTabNewsFragment();
+			break;
+		case 4:
+			tempfragment = new BBSInTabPostFragment();
+			break;
 		}
 		if (tempfragment != null) {
-			transaction.replace(R.id.bbs_tab_content, tempfragment);
+			transaction.replace(R.id.bbs_in_tab_content, tempfragment);
     	transaction.commit();
         }
 	}
--- src/com/edu/thss/smartdental/BBSTabChooseFragment.java
+++ src/com/edu/thss/smartdental/BBSTabChooseFragment.java
@@ -2,12 +2,18 @@ package com.edu.thss.smartdental;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.RadioGroup;
+import android.widget.Button;
 public class BBSTabChooseFragment extends Fragment {
 
+	private FragmentManager fragmentManager;
+	private Button choosing;
+	
 	public BBSTabChooseFragment() {
 		
 	}
@@ -15,8 +21,24 @@ public class BBSTabChooseFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_bbs_choose, container, false);
-		
+		fragmentManager = getFragmentManager();
+		choosing = (Button)rootView.findViewById(R.id.bbs_choose_button1);
+		choosing.setOnClickListener(new View.OnClickListener(){
+			 public void onClick(View v) {
+				// FragmentTransaction transaction = fragmentManager.beginTransaction();
+					Fragment fragment= new BBSInFragment();
+					//FragmentManager fragmentManager = getSupportFragmentManager();
+					fragmentManager.beginTransaction().replace(R.id.content_frame,fragment).commit();
+					
+					/*mDrawerList.setItemChecked(position, true);
+					mDrawerList.setSelection(position);
+					setTitle(mNavMenuTitles[position]);
+					mDrawerLayout.closeDrawer(mDrawerList);*/
+			 }
+		});
 		return rootView;
 	}
+	
+	//fragmentManager.beginTransaction().replace(R.id.content_frame,fragment).commit();
 	
 }
