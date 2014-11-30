c9d892dfdb15f92ad9a8788b1a3fbf44646335de
Tarma <tarmafu@hotmail.com>
2014-11-30 21:57:51 +0800

    Add circle list in Choose circle fragment

--- src/com/edu/thss/smartdental/BBSTabChooseFragment.java
+++ src/com/edu/thss/smartdental/BBSTabChooseFragment.java
@@ -1,5 +1,10 @@
 package com.edu.thss.smartdental;
 
+import java.util.ArrayList;
+
+import com.edu.thss.smartdental.adapter.CircleListAdapter;
+import com.edu.thss.smartdental.model.EMRElement;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,12 +12,15 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RadioGroup;
+import android.widget.ListView;
 import android.widget.Button;
 public class BBSTabChooseFragment extends Fragment {
 
 	private FragmentManager fragmentManager;
 	private Button choosing;
+	private ListView list;
+	private ArrayList<String> circles;
+	private CircleListAdapter listAdapter;
 	
 	public BBSTabChooseFragment() {
 		
@@ -36,7 +44,19 @@ public class BBSTabChooseFragment extends Fragment {
 					mDrawerLayout.closeDrawer(mDrawerList);*/
 			 }
 		});
+		
+		list = (ListView) rootView.findViewById(R.id.circle_list);
+		initCircles();
+		listAdapter = new CircleListAdapter(circles, this.getActivity().getApplicationContext());
+		list.setAdapter(listAdapter);
+		
 		return rootView;
+	}
+	
+	private void initCircles(){
+		circles = new ArrayList<String>();
+		circles.add("老司机");
+		circles.add("陈鸣海医生");
 	}
 	
 	//fragmentManager.beginTransaction().replace(R.id.content_frame,fragment).commit();
