c59b166522317c3c4e9160b8858cc5b2e5a46b9a
nezharen <nezharen@163.com>
2014-12-02 21:46:39 +0800

    注册功能

--- src/com/edu/thss/smartdental/LoginActivity.java
+++ src/com/edu/thss/smartdental/LoginActivity.java
@@ -9,13 +9,15 @@ import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
-	Button login;
+	Button login_btn, register_btn;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		login = (Button)findViewById(R.id.login_btn);
-		login.setOnClickListener(loginListener);
+		login_btn = (Button)findViewById(R.id.login_btn);
+		login_btn.setOnClickListener(loginListener);
+		register_btn = (Button)findViewById(R.id.register_btn);
+		register_btn.setOnClickListener(registerListener);
 	}
 	
 	private OnClickListener loginListener = new OnClickListener() {
@@ -24,6 +26,13 @@ public class LoginActivity extends Activity {
 			intent.setClass(LoginActivity.this, MainActivity.class);
 			startActivity(intent);
 			finish();
+		}
+	};
+	private OnClickListener registerListener = new OnClickListener() {
+		public void onClick(View v) {
+			Intent intent = new Intent();
+			intent.setClass(LoginActivity.this, RegisterActivity.class);
+			startActivity(intent);
 		}
 	};
 }
---
+++ src/com/edu/thss/smartdental/RegisterActivity.java
@@ -0,0 +1,53 @@
+package com.edu.thss.smartdental;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class RegisterActivity extends Activity {
+
+	Button register_btn, login_btn;
+	EditText username_edit, password_edit, repeat_password_edit;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		username_edit = (EditText)findViewById(R.id.username_edit);
+		password_edit = (EditText)findViewById(R.id.password_edit);
+		repeat_password_edit = (EditText)findViewById(R.id.repeat_password_edit);
+		register_btn = (Button)findViewById(R.id.register_btn);
+		register_btn.setOnClickListener(registerListener);
+		login_btn = (Button)findViewById(R.id.login_btn);
+		login_btn.setOnClickListener(loginListener);
+	}
+	
+	private OnClickListener registerListener = new OnClickListener() {
+		public void onClick(View v) {
+			String username = username_edit.getText().toString(), password = password_edit.getText().toString(), repeat_password = repeat_password_edit.getText().toString();
+			if (username.equals("")) {
+				Toast.makeText(RegisterActivity.this, "用户名不能为空", Toast.LENGTH_LONG).show();
+				return;
+			}
+			if (password.equals("")) {
+				Toast.makeText(RegisterActivity.this, "密码不能为空", Toast.LENGTH_LONG).show();
+				return;
+			}
+			if (!repeat_password.equals(password)) {
+				Toast.makeText(RegisterActivity.this, "两次密码输入不一致", Toast.LENGTH_LONG).show();
+				return;
+			}
+			Toast.makeText(RegisterActivity.this, "注册成功，请登录", Toast.LENGTH_LONG).show();
+			finish();
+		}
+	};
+	
+	private OnClickListener loginListener = new OnClickListener() {
+		public void onClick(View v) {
+			finish();
+		}
+	};
+}
