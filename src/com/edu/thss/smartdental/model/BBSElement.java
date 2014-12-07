229ebe18afd629a8e524d81e48b75f55a11d448e
Ontree <wangyicmh@163.com>
2014-12-07 19:59:28 +0800

    preview提取

--- src/com/edu/thss/smartdental/model/BBSElement.java
+++ src/com/edu/thss/smartdental/model/BBSElement.java
@@ -3,15 +3,17 @@ package com.edu.thss.smartdental.model;
 public class BBSElement {
 	public String title;
 	public String preview;
+	public String content;
 	public String time;
 	public String tab;
 	public String author;
 	public boolean isCollected;
 	public boolean isDeletable;
 	
-	public BBSElement(String title,String preview, String time, String author, boolean isCollected, boolean isDeletable){
+	public BBSElement(String title,String content, String time, String author, boolean isCollected, boolean isDeletable){
 		this.title = title;
-		this.preview = preview;
+		this.content = content;
+		this.preview = content.substring(0, 20);
 		this.time =time;
 		this.tab = "";
 		this.author = author;
