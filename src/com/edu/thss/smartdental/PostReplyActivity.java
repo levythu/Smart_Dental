848c4a6af9bbb0d450c6d312ee6808cbbdad4015
THU-wyw <wyw0714@gmail.com>
2014-12-18 16:40:57 +0800

    完成发表评论Activity的框架

--- src/com/edu/thss/smartdental/PostReplyActivity.java
+++ src/com/edu/thss/smartdental/PostReplyActivity.java
@@ -1,16 +1,20 @@
 package com.edu.thss.smartdental;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 
 public class PostReplyActivity extends Activity {
-	
+	private ActionBar actionBar;
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.activity_post);
 	}
 	
@@ -18,6 +22,21 @@ public class PostReplyActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu){
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.post_menu, menu);
+		return true;
+	}
+	
+	@Override 
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			break;
+		case R.id.cancel_button:
+			finish();
+			break;
+		default:
+			break;
+		}
 		return true;
 	}
 }
