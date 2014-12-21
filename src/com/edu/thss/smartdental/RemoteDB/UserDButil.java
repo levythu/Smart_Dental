e13991394d2bbb4667f57f64888d88cbf732f8fa
tionry <tionry@163.com>
2014-12-21 14:55:59 +0800

    code Refactoring

---
+++ src/com/edu/thss/smartdental/RemoteDB/UserDButil.java
@@ -0,0 +1,19 @@
+/*
+ * AuthorBy:qiaocy
+ * 
+ */
+package com.edu.thss.smartdental.RemoteDB;
+
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserDButil {
+	private ArrayList<String> namespace = new ArrayList<String>();
+	private ArrayList<String> parameter = new ArrayList<String>();
+	private ArrayList<String> resultarray = new ArrayList<String>();
+	private HttpConnSoap Soap = new HttpConnSoap();
+	
+}
