353603f6d9f1933b496cddee164b05a2d098c3ed
nezharen <nezharen@163.com>
2014-12-25 16:28:15 +0800

    all_patient

---
+++ src/com/edu/thss/smartdental/AllPatientActivity.java
@@ -0,0 +1,11 @@
+package com.edu.thss.smartdental;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AllPatientActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_all_patient);
+	}
+}
