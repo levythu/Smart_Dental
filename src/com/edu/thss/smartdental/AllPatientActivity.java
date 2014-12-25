4ea4a57d5de62fdf7be2b148d4ecd65598f554a5
nezharen <nezharen@163.com>
2014-12-25 21:01:18 +0800

    kickout

--- src/com/edu/thss/smartdental/AllPatientActivity.java
+++ src/com/edu/thss/smartdental/AllPatientActivity.java
@@ -5,15 +5,19 @@ import java.util.List;
 import java.util.ArrayList;
 import android.app.Activity;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
-import android.content.SharedPreferences;
-import android.view.View;
 import android.widget.Toast;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.view.MenuItem;
+import android.view.View;
 import com.edu.thss.smartdental.RemoteDB.UserDBUtil;
+
 
 public class AllPatientActivity extends Activity {
 
@@ -22,6 +26,7 @@ public class AllPatientActivity extends Activity {
 	SharedPreferences preferences = null;
 	ActionBar actionBar;
 	List<HashMap<String, String>> list;
+	int tPosition;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,11 +41,42 @@ public class AllPatientActivity extends Activity {
 		}
 		allpatient = (ListView)findViewById(R.id.all_patient);
 		allpatient.setAdapter(new ArrayAdapter<String>(AllPatientActivity.this, android.R.layout.simple_list_item_1, data));
-		allpatient.setOnItemLongClickListener(new OnItemLongClickListener() {
-			public boolean onItemLongClick(AdapterView<?>adapterView, View view, int position, long id) {
-				Toast.makeText(AllPatientActivity.this,"您选择了" + position, Toast.LENGTH_LONG).show();
-				return true;
-			}
-		});
+		allpatient.setOnItemLongClickListener(patientListener);
 	}
+	
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			break;
+		}
+		return true;
+	}
+	
+	OnItemLongClickListener patientListener = new OnItemLongClickListener() {
+		public boolean onItemLongClick(AdapterView<?>adapterView, View view, int position, long id) {
+			tPosition = position;
+			AlertDialog.Builder builder = new AlertDialog.Builder(AllPatientActivity.this);
+			builder.setIcon(R.drawable.ic_launcher);
+			builder.setTitle("确认");
+			builder.setMessage("您真的要将" + list.get(position + 1).get("username") + "踢出圈子吗？");
+			builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					Toast.makeText(AllPatientActivity.this, db.kickout(list.get(tPosition + 1).get("username"), preferences.getString("username", "")), Toast.LENGTH_LONG).show();
+					list = db.getAllpatients(preferences.getString("username", ""));
+					ArrayList<String> data = new ArrayList<String>();
+					for (int i = 1; i < list.size(); i++) {
+						data.add(list.get(i).get("username"));
+					}
+					allpatient.setAdapter(new ArrayAdapter<String>(AllPatientActivity.this, android.R.layout.simple_list_item_1, data));
+				}
+			});
+			builder.setNegativeButton("取消", null);
+			builder.show();
+			return true;
+		}
+	};
 }
